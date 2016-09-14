0b4c73bdc1821fca693c819c32d5f676b6176d0e
regikriv <regikriv@greeny.cs.tlu.ee>
2016-09-14 08:54:30 +0300

    Sõnum muudatusest

---
+++ helloworld.php
@@ -0,0 +1,13 @@
+<?php
+
+	$firstName = "Regiina" ;
+	$lastName = "Krivulina" ;
+
+	// echo trükib välja muutuja väärtuse
+	// echo $firstName;
+	// echo $lastName;
+	
+	echo $firstName. " " .$lastName;
+	//punkt liidab stringe, tühiku saab tühjade jutumärkidega
+	
+?>
